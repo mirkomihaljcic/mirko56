2142e1f6499e09f394cc8d1e853a5fd8423e1fa0
mirkomihaljcic <mirko.mihaljcic@apeiron-edu.eu>
2020-11-16 05:57:33 +0100

    updated changes

--- index.php
+++ index.php
@@ -1,17 +1,51 @@
 <html>
 <head>  
-<link rel="stylesheet" href="styles.css">
 <Title>Prakticni</Title>  
-</head>  
+</head>
+
+<style>
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type=submit] {
+  width: 100%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+div {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+</style>
+
+
 <body>  
 <div>
 <form method="post" action="?action=add" > 
 <label for="rednibroj">Redni Broj:</label>
 <input type="text" name="t_rednib" id="t_rednib" placeholder="Redni Broj">
 <label for="ime">Ime:</label>
-<input type="text" name="t_ime" id="t_ime"placeholder="Ime"> 
+<input type="text" name="t_ime" id="t_ime" placeholder="Ime"> 
 <label for="prezime">Prezime:</label>
-<input type="text" name="t_prezime" id="t_prezime"placeholder="Prezime">
+<input type="text" name="t_prezime" id="t_prezime" placeholder="Prezime">
 <input type="submit" name="submit" value="Unesi" />  
 </form> 
 </div> 
