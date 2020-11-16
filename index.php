aa7f3daf3440a3954403b540b16fa2b33c48bef5
mirkomihaljcic <mirko.mihaljcic@apeiron-edu.eu>
2020-11-16 05:02:28 +0100

    updated changes

--- index.php
+++ index.php
@@ -1,20 +1,38 @@
-<html><head><Title>Primer Azure SQL Databaze sa PHP web stranom</Title></head><body>  
-<form method="post" action="?action=add">  
-RB      <input type="text" name="t_rb"      id="t_rb"/></br>  
-IME     <input type="text" name="t_ime"     id="t_ime"/></br>  
-PREZIME <input type="text" name="t_prezime" id="t_prezime"/></br>  
-<input type="submit" name="submit" value="Submit"/></form>  
-<?php      
-$serverName = "tcp:servermirko55.database.windows.net,1433";  
-$connectionOptions = array("Database" => "mirko56database","UID" => "mirkoazure","PWD" => "Mirko1234567*");  
-$conn = sqlsrv_connect($serverName, $connectionOptions); 
 
+
+<html>  
+<head>  
+<Title>Azure SQL Database - PHP Website</Title>  
+</head>  
+<body>  
+<form method="post" action="?action=add" enctype="multipart/form-data" >  
+Emp Id <input type="text" name="t_emp_id" id="t_emp_id"/></br>  
+Name <input type="text" name="t_name" id="t_name"/></br>  
+Education <input type="text" name="t_education" id="t_education"/></br>  
+E-mail address <input type="text" name="t_email" id="t_email"/></br>  
+<input type="submit" name="submit" value="Submit" />  
+</form>  
+<?php  
+
+
+$serverName = "tcp:servermirko56.database.windows.net,1433";  
+$connectionOptions = array("Database" => "mirko56database","UID" => "mirkoazure","PWD" => "Mirko1234567*");  
+$conn = sqlsrv_connect($serverName, $connectionOptions);  
+  
+if ($conn === false)  
+    {  
+    die(print_r(sqlsrv_errors() , true));  
+    }  
+  
 if (isset($_GET['action']))  
     {  
     if ($_GET['action'] == 'add')  
-        {   
-        $insertSql = "INSERT INTO tabela (RB,IME,PREZIME) VALUES (?,?,?)";  
-        $params = array(&$_POST['t_rb'], &$_POST['t_ime'], &$_POST['t_prezime']);  
+        {  
+        /*Insert data.*/  
+        $insertSql = "INSERT INTO empTable (emp_id,name,education,email)   
+VALUES (?,?,?,?)";  
+        $params = array(&$_POST['t_emp_id'], &$_POST['t_name'], &$_POST['t_education'], &$_POST['t_email']  
+        );  
         $stmt = sqlsrv_query($conn, $insertSql, $params);  
         if ($stmt === false)  
             {  
@@ -36,10 +54,35 @@ if (isset($_GET['action']))
             echo "Registration complete.</br>";  
             }  
         }  
-    }
-
-
-
-
-
-?></body></html>
+    }  
+  
+/*Display registered people.*/  
+$sql = "SELECT * FROM empTable ORDER BY name"; 
+$stmt = sqlsrv_query($conn, $sql); 
+if($stmt === false) 
+{ 
+die(print_r(sqlsrv_errors(), true)); 
+} 
+ 
+if(sqlsrv_has_rows($stmt)) 
+{ 
+print("<table border='1px'>"); 
+print("<tr><td>Emp Id</td>"); 
+print("<td>Name</td>"); 
+print("<td>education</td>"); 
+print("<td>Email</td></tr>"); 
+ 
+while($row = sqlsrv_fetch_array($stmt)) 
+{ 
+ 
+print("<tr><td>".$row['emp_id']."</td>"); 
+print("<td>".$row['name']."</td>"); 
+print("<td>".$row['education']."</td>"); 
+print("<td>".$row['email']."</td></tr>"); 
+} 
+ 
+print("</table>"); 
+}  
+?>  
+</body>  
+</html>  
