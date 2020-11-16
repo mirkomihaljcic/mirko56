02a1c0c6dbf34ae3005fc5cd94e6cdf3591d98ba
mirkomihaljcic <mirko.mihaljcic@apeiron-edu.eu>
2020-11-16 04:47:40 +0100

    updated changes

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
-</html>
+<html><head><Title>Primer Azure SQL Databaze sa PHP web stranom</Title></head><body>  
+<?php      
+$serverName = "tcp:servermirko55.database.windows.net,1433";  
+$connectionOptions = array("Database" => "mirko55database","UID" => "mirkofuckazure","PWD" => "Mirko1234567*");  
+$conn = sqlsrv_connect($serverName, $connectionOptions); 
+
+
+
+
+	 
+	echo "New record created successfully !";
+
+
+
+
+    
+?></body></html>
