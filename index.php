55e27de5f3c6a56230314ade05f2a64b80f4224c
mirkomihaljcic <mirko.mihaljcic@apeiron-edu.eu>
2020-11-16 04:50:48 +0100

    updated changes

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <html><head><Title>Primer Azure SQL Databaze sa PHP web stranom</Title></head><body>  
 <form method="post" action="?action=add">  
-RB <input type="text" name="t_rb" id="t_rb"/></br>  
-IME <input type="text" name="t_ime" id="t_ime"/></br>  
+RB      <input type="text" name="t_rb"      id="t_rb"/></br>  
+IME     <input type="text" name="t_ime"     id="t_ime"/></br>  
 PREZIME <input type="text" name="t_prezime" id="t_prezime"/></br>  
-<input type="submit" name="submit" value="Submit" />  
-</form>  
-
+<input type="submit" name="submit" value="Submit"/></form>  
 <?php      
 $serverName = "tcp:servermirko55.database.windows.net,1433";  
 $connectionOptions = array("Database" => "mirko55database","UID" => "mirkofuckazure","PWD" => "Mirko1234567*");  
